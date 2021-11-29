4dd0df02d9f4a3c25a00ea35111cc09290fcef32
Merly <merly2257@gmail.com>
2021-11-29 11:26:53 -0500

    se paso de html a doc firebase.js

--- src/view/login.js
+++ src/view/login.js
@@ -1,5 +1,5 @@
 import {
-  getAuth, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword,
+  getAuth, signInWithPopup, GoogleAuthProvider, signInWithEmailAndPassword, FacebookAuthProvider,
 // eslint-disable-next-line import/no-unresolved
 } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-auth.js';
 
@@ -78,6 +78,34 @@ export const initLogin = () => {
         // // The AuthCredential type that was used.
         // const credential = GoogleAuthProvider.credentialFromError(error);
         // ...
+        console.log(error);
+      });
+  });
+
+  document.querySelector('#facebookLogin').addEventListener('click', () => {
+    const provider = new FacebookAuthProvider();
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // The signed-in user info.
+        const user = result.user;
+
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        const credential = FacebookAuthProvider.credentialFromResult(result);
+        const accessToken = credential.accessToken;
+
+        // ...
+      })
+      .catch((error) => {
+        // // Handle Errors here.
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+        // // The email of the user's account used.
+        // const email = error.email;
+        // // The AuthCredential type that was used.
+        // const credential = FacebookAuthProvider.credentialFromError(error);
+
+        // // ...
         console.log(error);
       });
   });
