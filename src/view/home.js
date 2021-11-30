c8351ba3ebaaeafaa8dc0b1908ebdfb4c367ac9d
nicolmendoza <nicolmendozamattos@gmail.com>
2021-11-30 06:58:02 -0500

    añadiemdo info usuario

--- src/view/home.js
+++ src/view/home.js
@@ -1,18 +1,34 @@
 // eslint-disable-next-line import/no-unresolved
-import { getAuth, signOut } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-auth.js';
+import { getAuth, signOut, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-auth.js';
 
 export const Home = () => {
   const divElement = document.createElement('div');
   divElement.innerHTML = ` 
   <div>WELCOME</div>
+  <div id="infoUser"></div>
+  <img id="photoUser">
   <button id="logout">Log Out</button>`;
 
   return divElement;
 };
 
 export const LogOut = () => {
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      // const uid = user.uid;
+      console.log(user);
+      document.getElementById('infoUser').innerHTML = `Nombre:${user.displayName} Correo=${user.email}`;
+      document.getElementById('photoUser').src = `${user.photoURL}`;
+    } else {
+      // User is signed out
+      // ...
+    }
+  });
+
   document.querySelector('#logout').addEventListener('click', () => {
-    const auth = getAuth();
     signOut(auth)
       .then(() => {
         console.log('log out');
