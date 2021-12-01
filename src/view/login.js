88f0cfc0cbd057f0852091f9aed02b3d52beaee1
andreahenos <henostroza.andrea@gmail.com>
2021-11-30 21:03:57 -0500

    Redireccionamineto en Login with GitHub

--- src/view/login.js
+++ src/view/login.js
@@ -124,12 +124,16 @@ export const initLogin = () => {
     const auth = getAuth();
     signInWithPopup(auth, provider)
       .then((result) => {
+        console.log('github sign in');
+        window.location.hash = '#/home';
         // This gives you a GitHub Access Token. You can use it to access the GitHub API.
         // const credential = GithubAuthProvider.credentialFromResult(result);
         // const token = credential.accessToken;
 
         // The signed-in user info.
-        // const user = result.user;
+        const user = result.user;
+        console.log(user);
+        alert(`Bienvenida ${user.displayName}`);
         // ...
       }).catch((error) => {
         // Handle Errors here.
