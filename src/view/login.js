c7597266ffb6ce89d186e5cd1b3da13cf4fcd631
andreahenos <henostroza.andrea@gmail.com>
2021-12-10 20:50:51 -0500

    Modularizar 8 (10/12)

--- src/view/login.js
+++ src/view/login.js
@@ -10,9 +10,10 @@ export const Login = () => {
   viewHome.classList.add('sectionLogin');
   viewHome.innerHTML = `
   <section class="bubble">
-  <!-- content here -->
+    <!-- content here -->
   </section>
-  <div class="section1"><img class="imgInicio" ></div>
+
+  <div class="section1"><img class="imgInicio"></div>
   <div class="section2">
     <div class="formulario">
       <div class="logo">SOCIAL NETWORK</div>
@@ -32,20 +33,21 @@ export const Login = () => {
           <span class="icon-input">
             <i class="far fa-envelope"></i>
           </span>
-          <input type="email" id="login-email"  placeholder="correo@example.com" required >
+          <input type="email" id="login-email" placeholder="correo@example.com" required>
         </div>
 
         <div class="form-group">
           <span class="icon-input" id="icon-eye">
             <i class="fas fa-eye-slash"></i>
           </span>
-          <input type="password" id="login-password"  placeholder="****************" required >
+          <input type="password" id="login-password"  placeholder="****************" required>
         </div>
 
         <p id="generalMessage" class="errorMessage"></p>
         <button type="submit" class="btnLogin">LOGIN</button><br>
         <div class="divResetPassword">
-        <a href="#/resetPassword" id="resetPass"> Forgot password?</a></div>
+          <a href="#/resetPassword" id="resetPass"> Forgot password?</a>
+        </div>
         
         <button type="button" class="icon-login" id="googleLogin"><img src="img/google.png"></i></button>
         <button type="button" class="icon-login" id="facebookLogin"><img src="img/facebook.png"></button>
@@ -62,8 +64,6 @@ export const Login = () => {
 
 export const initLogin = () => {
   const signInForm = document.querySelector('#login-form');
-  const userEmail = document.querySelector('#login-email').value;
-  const userPassword = document.querySelector('#login-password').value;
   /* ................................... */
   const googleLogin = document.querySelector('#googleLogin');
   const facebookLogin = document.querySelector('#facebookLogin');
@@ -71,57 +71,51 @@ export const initLogin = () => {
   /* ................................... */
   const message = document.getElementById('generalMessage');
 
-  /* ........Imprimir errores....... */
-  // function errorOccurs(typeError) {
-  //   const errorCode = typeError.code;
-  //   switch (errorCode) {
-  //     case 'auth/user-not-found':
-  //       message.innerHTML = 'Usuario no encontrado';
-  //       break;
-  //     case 'auth/wrong-password':
-  //       message.innerHTML = 'Contraseña incorrecta.';
-  //       break;
-  //     case 'auth/too-many-requests':
-  // eslint-disable-next-line max-len
-  //       message.innerHTML = 'Usted excedió el número de intentos fallidos. Reestablezca su contraseña o inténtelo más tarde.';
-  //       break;
-  //     case 'auth/invalid-email':
-  //       message.innerHTML = 'La dirección de correo electrónico no es válida';
-  //       break;
-  //     default:
-  //       message.innerHTML = 'Lo sentimos, se ha producido un error en la página.';
-  //   }
-  // }
+  /* ........Ingreso correcto....... */
+  function correctEntry() {
+    window.location.hash = '#/home';
+  }
+
+  /* ........Errorres para ingresar....... */
+  function errorOccurs(typeError) {
+    const errorCode = typeError.code;
+    switch (errorCode) {
+      case 'auth/user-not-found':
+        message.innerHTML = 'Usuario no encontrado';
+        break;
+      case 'auth/wrong-password':
+        message.innerHTML = 'Contraseña incorrecta.';
+        break;
+      case 'auth/too-many-requests':
+        message.innerHTML = 'Usted excedió el número de intentos fallidos. Reestablezca su contraseña o inténtelo más tarde.';
+        break;
+      case 'auth/invalid-email':
+        message.innerHTML = 'La dirección de correo electrónico no es válida';
+        break;
+      default:
+        message.innerHTML = 'Lo sentimos, se ha producido un error en la página.';
+    }
+  }
 
   /* .........Logearse con correo........ */
   signInForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    const email = userEmail;
-    const password = userPassword;
+    const email = e.target.childNodes[1].childNodes[3].value;
+    // const email = document.querySelector('#login-email').value;
+    const password = e.target.childNodes[3].childNodes[3].value;
+    // const password = document.querySelector('#login-password').value;
 
     loginEmail(email, password)
       .then((userCredential) => {
-        if (userCredential.user.emailVerified) {
-          window.location.hash = '#/home';
+        const user = userCredential.user;
+        if (user.emailVerified === true) {
+          correctEntry();
         } else {
-          // eslint-disable-next-line no-alert
-          alert('Te enviamos un correo para verificar tu cuenta, por favor revisa tu bandeja.');
+          alert('Te hemos enviado un email para verificar tu cuenta. Por favor revisa tu bandeja.');
         }
       })
       .catch((error) => {
-        const errorMessage = error.message;
-        if (errorMessage === 'Firebase: Error (auth/user-not-found).') {
-          message.innerHTML = 'Usuario no encontrado';
-        }
-
-        if (errorMessage === 'Firebase: Error (auth/wrong-password).') {
-          message.innerHTML = 'Contraseña incorrecta.';
-        } else if (errorMessage === 'Firebase: Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later. (auth/too-many-requests).') {
-          message.innerHTML = 'Usted excedió el número de intentos fallidos. Reestablezca su contraseña o inténtelo más tarde.';
-        }
-        // const errorMessage = error.message;
-        // console.log(errorMessage);
-        // errorOccurs(error);
+        errorOccurs(error);
       });
   });
 
@@ -129,12 +123,9 @@ export const initLogin = () => {
   googleLogin.addEventListener('click', () => {
     loginGoogle()
       .then(() => {
-        window.location.hash = '#/home';
+        correctEntry();
       }).catch((error) => {
-        const errorMessage = error.message;
-        // eslint-disable-next-line no-console
-        console.log(errorMessage);
-        // errorOccurs(error);
+        errorOccurs(error);
       });
   });
 
@@ -142,13 +133,10 @@ export const initLogin = () => {
   facebookLogin.addEventListener('click', () => {
     loginFacebook()
       .then(() => {
-        window.location.hash = '#/home';
+        correctEntry();
       })
       .catch((error) => {
-        const errorMessage = error.message;
-        // eslint-disable-next-line no-console
-        console.log(errorMessage);
-        // errorOccurs(error);
+        errorOccurs(error);
       });
   });
 
@@ -156,18 +144,14 @@ export const initLogin = () => {
   githubLogin.addEventListener('click', () => {
     loginGitHub()
       .then(() => {
-        window.location.hash = '#/home';
+        correctEntry();
       }).catch((error) => {
-        const errorMessage = error.message;
-        // eslint-disable-next-line no-console
-        console.log(errorMessage);
-        // errorOccurs(error);
+        errorOccurs(error);
       });
   });
 
   /* .....Función ocultar y mostrar contraseña..... */
   const iconEye = document.querySelector('#icon-eye');
-
   // eslint-disable-next-line func-names
   iconEye.addEventListener('click', function () {
     const icon = this.querySelector('i');
