ffaff8826ea6a6bdac98546fcd774dc6263e0068
andreahenos <henostroza.andrea@gmail.com>
2021-12-14 15:09:13 -0500

    Corrección error

--- src/view/post.js
+++ src/view/post.js
@@ -2,45 +2,37 @@
 // import {
 //   getFirestore, collection, addDoc,
 // } from 'https://www.gstatic.com/firebasejs/9.5.0/firebase-firestore.js';
-
 import {
   deletePost,
   currentUser, obtenerInfo, updatePost, readComment, saveComment, updateLikePost,
 } from '../firebase.js';
-
 import { templateComents }
   from './comments.js';
-
 export const template = (post) => {
   console.log(post);
   const showPost = document.getElementById('showPost');
   const nuevoElemento = document.createElement('div');
   const user = currentUser().currentUser;
-
   const postElements = post.map(async (onePost) => {
     const dataUser = await obtenerInfo(onePost.userID);
-
     const arrOfUsers = onePost.likes[0].users;
     const likeIcon = arrOfUsers.includes(user.uid) ? 'fas' : '';
-
     nuevoElemento.innerHTML += `
     <div class="postDiv" id="${onePost.idP}">
       <div class="header-post">
-
         <img src=${dataUser.data().photo} width="100px" >
         <div class="header-info">
           <div class="post-name">${dataUser.data().name}</div>
-          <div class="date"><p></p></div> 
+          <div class="date"><p></p></div>
         </div>
       </div>
-      <div class="text-post-home" "id="contentPost${onePost.idP}">${onePost.content}</div>
+      <div class="text-post-home" id="contentPost${onePost.idP}">${onePost.content}</div>
       <img class="postImg" id="img-${onePost.idP}" src="${onePost.img}">
       <button class="delete">DELETE</button>
       <button class="edit">EDIT</button>
       <div id="postIcon">
           <i class="${likeIcon} far fa-heart icon"></i> <p class='cant'>${onePost.likes[0].users.length}</p>
           <i class="far fa-comment icon"></i><p class="countComment${onePost.idP}"></p>
-
           <i class="far fa-paper-plane icon"></i>
       </div>
       <div id="comments${onePost.idP}">
@@ -48,13 +40,10 @@ export const template = (post) => {
         <div id="showComment${onePost.idP}"></div>
       </div>
     </div>`;
-
     return nuevoElemento;
   });
-
   Promise.all(postElements).then(() => {
     const uidUser = (user.uid);
-
     post.forEach((one) => {
       const idPost = one.idP;
       const parrafoCountComment = nuevoElemento.querySelector(`.countComment${idPost}`);
@@ -63,7 +52,6 @@ export const template = (post) => {
         parrafoCountComment.innerHTML = num;
       }, idPost);
     });
-
     nuevoElemento.querySelectorAll('.postImg').forEach((postImg) => {
       const postImgId = postImg.parentElement.id;
       console.log(postImgId);
@@ -78,7 +66,6 @@ export const template = (post) => {
         }
       }
     });
-
     nuevoElemento.querySelectorAll('.date').forEach((date) => {
       const postId = date.parentElement.parentElement.parentElement.id;
       const pElement = date.firstChild;
@@ -89,19 +76,16 @@ export const template = (post) => {
           d.setTime(dateUTC);
           const dateData = d.toLocaleDateString();
           // console.log(dateData, i);
-
           pElement.innerHTML = dateData;
         }
       }
     });
-
     nuevoElemento.querySelectorAll('.delete').forEach((div) => {
       div.addEventListener('click', (e) => {
         console.log('hi');
         console.log(e.target.parentNode);
         const id = e.target.parentNode.id;
         console.log(id);
-
         for (let i = 0; i < post.length; i++) {
           console.log(post[i].userID === user.uid, post[i].idP === id);
           if (post[i].userID === user.uid && post[i].idP === id) {
@@ -115,29 +99,24 @@ export const template = (post) => {
         }
       });
     });
-
     nuevoElemento.querySelectorAll('.edit').forEach((div) => {
       div.addEventListener('click', (e) => {
         const id = e.target.parentNode.id;
-        console.log(id);
-
         for (let i = 0; i < post.length; i++) {
-          console.log(post[i].userID === user.uid, post[i].idP === id);
           if (post[i].userID === user.uid && post[i].idP === id) {
+            console.log(document.querySelector(`#contentPost${id}`));
             document.querySelector(`#contentPost${post[i].idP}`).innerHTML = `<textarea id="contentEdit${post[i].idP}"}>${post[i].content}</textarea>
             <button class="save">SAVE</button>`;
             document.querySelector('.save').addEventListener('click', () => {
-              const postEdit = document.getElementById(`contentEdit${post[i].idP}`).value;
+              const postEdit = document.getElementById(`contentEdit${id}`).value;
               if (postEdit === `${post[i].content}`) {
-                document.querySelector(`#contentPost${post[i].idP}`).innerHTML = `${post[i].content}`;
+                document.querySelector(`#contentPost${id}`).innerHTML = `${post[i].content}`;
               } else {
-                console.log(document.getElementById(`contentEdit${post[i].idP}`).value);
+                console.log(document.getElementById(`contentEdit${id}`).value);
                 updatePost(id, postEdit);
               }
-
               updatePost(id, postEdit);
             });
-
             break;
           // } else {
           //   alert('no puedes borrar un post ajeno');
@@ -146,21 +125,18 @@ export const template = (post) => {
         }
       });
     });
-
     nuevoElemento.querySelectorAll('.fa-heart').forEach((like) => {
       like.addEventListener('click', (e) => {
         const postId = e.target.parentNode.parentNode.id;
         const currentPost = post.filter((postElement) => postElement.idP === postId);
         const arrOfUsers = currentPost[0].likes[0].users;
         const cant = e.target.nextElementSibling;
-
         function removeItemFromArr(arr, item) {
           const j = arr.indexOf(item);
           if (j !== -1) {
             arr.splice(j, 1);
           }
         }
-
         if (arrOfUsers.includes(user.uid)) {
           e.target.classList.toggle('fas');
           removeItemFromArr(arrOfUsers, user.uid);
@@ -181,14 +157,12 @@ export const template = (post) => {
         cant.innerHTML = arrOfUsers.length;
       });
     });
-
     nuevoElemento.querySelectorAll('.fa-comment').forEach((icon) => {
       icon.addEventListener('click', (e) => {
         const id = e.target.parentNode.parentNode.id;
         const showComment = document.getElementById(`showComment${id}`);
         showComment.innerHTML = '';
         readComment(templateComents, id);
-
         const divComment = document.createElement('div');
         divComment.innerHTML = `<textarea id="textComent${id}"></textarea>
         <button id="saveComment${id}">SAVE</button>`;
@@ -207,7 +181,6 @@ export const template = (post) => {
         });
       });
     });
-
     showPost.innerHTML = '';
     showPost.appendChild(nuevoElemento);
   });
