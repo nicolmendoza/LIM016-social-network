fea7853e23e46bd83aca367997277d9f80910e24
Merly <merly2257@gmail.com>
2021-12-21 18:42:14 -0500

    intentando test firestore

--- src/firebase/__mocks__/firebase-config.js
+++ src/firebase/__mocks__/firebase-config.js
@@ -12,24 +12,32 @@
 //     displayName: '',
 //   },
 // });
-
-const GoogleAuthProvider = jest.fn(() => Promise.resolve('merly', 'merly@gmail.com'));
-const FacebookAuthProvider = jest.fn();
-const GithubAuthProvider = jest.fn();
-
-const signInWithPopup = jest.fn(() => Promise.resolve({}));
-
 const auth = {
   currentUser: {
     uid: 'fake-user-id',
   },
 };
+
+const GoogleAuthProvider = jest.fn(() => Promise.resolve({}));
+const FacebookAuthProvider = jest.fn();
+const GithubAuthProvider = jest.fn();
+
+const signInWithPopup = jest.fn(() => Promise.resolve({}));
+
 // export const setDoc = jest.fn((document, values) => Promise.resolve(values));
 // export const doc = jest.fn((db, collection, docId) => Promise.resolve({}));
 // export const db = {};
 
 // eslint-disable-next-line max-len
 const signInWithEmailAndPassword = jest.fn((auth, email, password) => Promise.resolve({}));
+
+const sendPasswordResetEmail = jest.fn((email) => Promise.resolve('merly2257@gmail.com'));
+
+// MOCK DE FIRESTORE
+
+export const collection = jest.fn((db, values) => Promise.resolve(values));
+export const addDoc = jest.fn((collection, db) => Promise.resolve({}));
+export const db = {};
 
 export {
   signInWithEmailAndPassword,
@@ -38,4 +46,5 @@ export {
   GithubAuthProvider,
   FacebookAuthProvider,
   GoogleAuthProvider,
+  sendPasswordResetEmail,
 };
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,41 +1,81 @@
 // importamos la funcion que vamos a testear
 // import { loginEmail, loginGoogle } from '../src/firebase/firebase-auth.js';
 
-import {
-  loginEmail, loginGoogle,
-} from '../src/firebase/firebase-auth';
+// import {
+//   loginEmail, loginGoogle,
+// } from '../src/firebase/firebase-auth';
+
+// import {
+//   signInWithEmailAndPassword,
+//   signInWithPopup,
+//   GoogleAuthProvider,
+//   auth,
+//   sendPasswordResetEmail,
+// } from '../src/firebase/firebase-config';
+
+// jest.mock('../src/firebase/firebase-config');
+
+// describe('myFunction', () => {
+//   it('debería ser una función', (done) => {
+//     const res = loginEmail('fake email', 'fake password');
+//     res.then(() => {
+//       console.log(signInWithEmailAndPassword.mock);
+//       expect(signInWithEmailAndPassword.mock.calls[0][1]).toBe('fake email');
+//       expect(signInWithEmailAndPassword.mock.calls[0][2]).toBe('fake password');
+//       done();
+//     });
+//   });
+//   it('falla si no recibe argumentos', () => {
+//   });
+// });
+
+// describe('Function', () => {
+//   it('debería ser una función', (done) => {
+//     const res = GoogleAuthProvider('merly2257@gmail.com');
+//     res.then(() => {
+//       expect(signInWithPopup.mock.calls[0][1]).toBe({});
+//     });
+//     done();
+//   });
+// });
+
+// describe('deberia ser una función', () => {
+//   it('debería ser una función', () => {
+//     const res = sendPasswordResetEmail(auth, 'merly2257@gmail.com');
+//     res.then(() => {
+//       console.log(sendPasswordResetEmail.mock);
+//       console.log(sendPasswordResetEmail.mock.calls);
+//       expect(sendPasswordResetEmail.mock.calls[0][1]).toBe('merly2257@gmail.com');
+//     });
+//   });
+// });
+import { savePost } from '../src/firebase/firestore.js';
 
 import {
-  signInWithEmailAndPassword,
-  signInWithPopup,
-  GoogleAuthProvider,
+  addDoc,
+  collection,
 } from '../src/firebase/firebase-config';
 
 jest.mock('../src/firebase/firebase-config');
 
-describe('myFunction', () => {
-  it('debería ser una función', (done) => {
-    const res = loginEmail('fake email', 'fake password');
-    res.then(() => {
-      console.log(signInWithEmailAndPassword.mock);
-      expect(signInWithEmailAndPassword.mock.calls[0][1]).toBe('fake email');
-      expect(signInWithEmailAndPassword.mock.calls[0][2]).toBe('fake password');
+describe('savePost', () => {
+  it('funciona', () => {
+    const result = savePost('Fake post', 'fake user', 'fake img', 'public', 'event');
+    expect(result).toBe({
+      message: 'Fake post',
+      userId: 'fake user',
+      img: 'fake img',
+      likes: [{
+        users: [],
+      }],
+      date: Date.now(),
+      privacity: 'public',
+      type: 'event',
     });
-    done();
-  });
-  it('falla si no recibe argumentos', () => {
-  });
-});
 
-describe('Function', () => {
-  it('debería ser una función', (done) => {
-    const res = loginGoogle(GoogleAuthProvider);
-    res.then(() => {
-      console.log(signInWithPopup.mock);
-      console.log(signInWithPopup.mock.calls);
-      expect(signInWithPopup.mock.calls[0][1]).toBe('merly');
-      expect(signInWithPopup.mock.calls[0][2]).toBe('merly@gmail.com');
-    });
-    done();
+    expect(collection.mock.calls[0][1]).toBe('post');
+    expect(collection.mock.calls[0][2]).toBe('fake-user-id');
+
+    expect(typeof addDoc.mock.calls[0][0]).toBe('object');
   });
 });
